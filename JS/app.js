f91629da45d2f1a9ad2ae42e48a026c3e93613f2
steven43533 <steven43533@gmail.com>
2021-10-23 22:42:10 -0400

    Added pseudocode for JS

---
+++ JS/app.js
@@ -0,0 +1,31 @@
+// variable to use canvas 
+// variable for announcer 
+// variable for length count of snake to change accordingly 
+
+
+
+// start game button
+
+
+// upon game ending announcer box showing players stats 
+
+// reset game button
+
+// constructor for snake
+
+// switch case to detect movement
+
+// constructor for snakes food
+
+// render snakes food on canvas
+
+// once snake eats food, food renders in new spot on canvas
+
+// function that adds length to the snake for every food ate
+
+// set canvas boundaries so snake can't go off map
+
+// function/rule so that snake can't go backwards(eat itself)
+
+
+
