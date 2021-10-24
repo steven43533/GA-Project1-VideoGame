3b644bef44c2638c49d05e929b33b29faa425e3f
steven43533 <steven43533@gmail.com>
2021-10-24 05:07:24 -0400

    Added buttons,moreJS, psuedocode

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,26 @@
-// variable to use canvas 
+// variable to use canvas and get ctx
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+// set the width and height of the canvas to use for boundaries and other stuff
+canvas.setAttribute('width', getComputedStyle(canvas)['width'])
+canvas.setAttribute('height', getComputedStyle(canvas)['height'])
+
+// start game button var
+const startButton = document.getElementById('startButton')
+
+// reset button var
+const resetButton = document.getElementById('resetButton')
+// startButton EL
+//startButton.addEventListener('click', )
+
+// resetButton EL
+//resetButton.addEventListener('click', )
+
 // variable for announcer 
+let announcer = document.getElementById('announcer')
+
 // variable for length count of snake to change accordingly 
-
-
-
-// start game button
 
 
 // upon game ending announcer box showing players stats 
@@ -14,6 +30,8 @@
 // constructor for snake
 
 // switch case to detect movement
+
+
 
 // constructor for snakes food
 
